f3b21df936741749bd784d03f19f5be9cd4b3191
SuSong <susong@qiyi.com>
2018-11-30 14:00:27 +0800

    新增配置类**testing

---
+++ ActionResponse/Class/Configration/ActionResponeHeader.h
@@ -0,0 +1,20 @@
+//
+//  ActionResponeHeader.h
+//  ActionResponse
+//
+//  Created by susong on 2018/11/30.
+//  Copyright © 2018 susong. All rights reserved.
+//
+
+#ifndef ActionResponeHeader_h
+#define ActionResponeHeader_h
+
+#import "SS_UIConfigration.h"
+#import "SS_AppConfiguration.h"
+#import "SS_NSConfiguration.h"
+#import "SS_CPUInfo.h"
+#import "SS_DisplayLinkLabel.h"
+#import "UIResponder+SS_Handler.h"
+#import "EventType.h"
+
+#endif /* ActionResponeHeader_h */
